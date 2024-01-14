ba08b10f12bc62e22b1ce185acbb40f7a98ccbc5
egreatsage <michaelcull9715@gmail.com>
2024-01-14 20:14:43 +0300

    navbar

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <div className='flex flex-col justify-center mt-9 items-center'>
              
-           <h1 className='text-[green] font-sans font-bold text-xl'> <span className='text-[orange] underline italic  '>Donate</span> something or give your sister this contact</h1>
+           <h1 className='text-[green] font-sans font-bold text-md'> <span className='text-[orange] underline italic  '>Donate</span> something or give your sister this contact</h1>
            <h1 className='text-2xl mt-9 underline'>0769375210</h1>
    
               <div className='my-12'>
@@ -19,7 +19,7 @@ export default function Home() {
 	<div class="👍"></div>
 </div>
               </div> 
-              <h1 className='bg-[red] px-5 py-3 text-xl font-bold rounded-md'>This site is under Development</h1>
+              <h1 className='bg-[red] px-5 py-3 text-md font-bold rounded-md'>This site is under Development</h1>
 
            <h1 className='text-md italic bold text-slate-400 my-6'>@Shadow</h1>
      </div>
